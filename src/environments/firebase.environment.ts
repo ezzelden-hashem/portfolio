f6864a44e2bf89fc3c7dab45de7572de8d19d22b
ezzelden_h <ezzelden.hashem@gmail.com>
2025-09-21 20:45:32 +0300

    firebase analytics setup

---
+++ src/environments/firebase.environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAWf4oaTbksDsmYGcNCkw7goyugi5Q2hvQ",
+  authDomain: "ezzelden-hashem.firebaseapp.com",
+  projectId: "ezzelden-hashem",
+  storageBucket: "ezzelden-hashem.firebasestorage.app",
+  messagingSenderId: "213945093492",
+  appId: "1:213945093492:web:3b66e43cf74074b1607021",
+  measurementId: "G-6GTXRZLZBX"
+};
