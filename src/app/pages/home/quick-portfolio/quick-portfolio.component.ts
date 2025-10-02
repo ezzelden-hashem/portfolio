bf3bc592752a74fecb3009b355dc098b904f3b72
ezzelden_h <ezzelden.hashem@gmail.com>
2025-10-02 14:02:55 +0300

    solving the quick portfolio cards issue

--- src/app/pages/home/quick-portfolio/quick-portfolio.component.ts
+++ src/app/pages/home/quick-portfolio/quick-portfolio.component.ts
@@ -1,16 +1,21 @@
 import { Component, inject } from '@angular/core';
-import { QuickPortfolioCardComponent } from "./quick-portfolio-card/quick-portfolio-card.component";
+import { QuickPortfolioCardComponent } from './quick-portfolio-card/quick-portfolio-card.component';
 import { PortfolioService } from '../../../services/portfolio/portfolio.service';
+import { Project } from '../../../services/portfolio/portfolio.model';
+import { project_password_wallet } from '../../../services/portfolio/mobile-app/password-wallet.project';
 
 @Component({
   selector: 'app-quick-portfolio',
   imports: [QuickPortfolioCardComponent],
   templateUrl: './quick-portfolio.component.html',
-  styleUrl: './quick-portfolio.component.css'
+  styleUrl: './quick-portfolio.component.css',
 })
 export class QuickPortfolioComponent {
   portfolioService = inject(PortfolioService);
-  quickProjectsCount = 6;
-  quickProjects = this.portfolioService.getRandomProjects(this.quickProjectsCount);
-  
+  mustContains: Project[] = [project_password_wallet];
+  quickProjectsCount = 5;
+  quickProjects = this.portfolioService.getRandomProjects(
+    this.quickProjectsCount,
+    this.mustContains
+  );
 }
--- src/app/services/portfolio/portfolio.service.ts
+++ src/app/services/portfolio/portfolio.service.ts
@@ -1,55 +1,59 @@
 import { Injectable } from '@angular/core';
 import { portfolio } from './portfolio.data';
-import { Category, CategoryInfo, Folder, FolderInfo, PortfolioObjectType, Project, ProjectInfo } from './portfolio.model';
+import {
+  Category,
+  CategoryInfo,
+  Folder,
+  FolderInfo,
+  PortfolioObjectType,
+  Project,
+  ProjectInfo,
+} from './portfolio.model';
 import { project_password_wallet } from './mobile-app/password-wallet.project';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PortfolioService {
   private portfolioObject = portfolio;
-  getCategoriesList(): CategoryInfo[]
-  {
+  getCategoriesList(): CategoryInfo[] {
     let categoriesList: CategoryInfo[] = [];
-    for (const category of this.portfolioObject)
-    {
+    for (const category of this.portfolioObject) {
       categoriesList.push({
         objectType: PortfolioObjectType.CategoryInfo,
         id: category.id,
         name: category.name,
         title: category.title,
-        icon: category.icon
-      })
+        icon: category.icon,
+      });
     }
     return categoriesList;
   }
-  getCategoryFolders(categoryName: string): FolderInfo[]
-  {
+  getCategoryFolders(categoryName: string): FolderInfo[] {
     let categoryFolders: FolderInfo[] = [];
-    const findResult = this.portfolioObject.find((category) => category.name === categoryName);
-    if (findResult)
-    {
-      for (const folder of findResult.folders)
-      {
+    const findResult = this.portfolioObject.find(
+      (category) => category.name === categoryName
+    );
+    if (findResult) {
+      for (const folder of findResult.folders) {
         categoryFolders.push({
           objectType: PortfolioObjectType.FolderInfo,
           id: folder.id,
           name: folder.name,
           title: folder.title,
-          icon: folder.icon
+          icon: folder.icon,
         });
       }
     }
     return categoryFolders;
   }
-  getCategoryProjects(categoryName: string): ProjectInfo[]
-  {
+  getCategoryProjects(categoryName: string): ProjectInfo[] {
     let categoryProjects: ProjectInfo[] = [];
-    const findResult = this.portfolioObject.find((category) => category.name === categoryName);
-    if (findResult)
-    {
-      for (const project of findResult.projects)
-      {
+    const findResult = this.portfolioObject.find(
+      (category) => category.name === categoryName
+    );
+    if (findResult) {
+      for (const project of findResult.projects) {
         categoryProjects.push({
           objectType: PortfolioObjectType.ProjectInfo,
           id: project.id,
@@ -57,29 +61,26 @@ export class PortfolioService {
           title: project.title,
           icon: project.icon,
           thumbnail: project.thumbnail,
-          description: project.description
+          description: project.description,
         });
       }
     }
     return categoryProjects;
   }
-  getFolderProjects(folderId: string): ProjectInfo[]
-  {
+  getFolderProjects(folderId: string): ProjectInfo[] {
     let folderProjects: ProjectInfo[] = [];
     let folder!: Folder;
-    for (const category of this.portfolioObject)
-    {
-      const findResult = category.folders.find(folder => folder.id === folderId)
-      if (findResult)
-      {
+    for (const category of this.portfolioObject) {
+      const findResult = category.folders.find(
+        (folder) => folder.id === folderId
+      );
+      if (findResult) {
         folder = findResult;
         break;
       }
     }
-    if (folder)
-    {
-      for (const project of folder.projects)
-      {
+    if (folder) {
+      for (const project of folder.projects) {
         folderProjects.push({
           objectType: PortfolioObjectType.ProjectInfo,
           id: project.id,
@@ -87,40 +88,40 @@ export class PortfolioService {
           title: project.title,
           icon: project.icon,
           thumbnail: project.thumbnail,
-          description: project.description
+          description: project.description,
         });
       }
     }
     return folderProjects;
   }
-  getCategoryById(categoryId: string): Category | null
-  {
-    const findResult = this.portfolioObject.find(c => c.id === categoryId);
+  getCategoryById(categoryId: string): Category | null {
+    const findResult = this.portfolioObject.find((c) => c.id === categoryId);
     if (findResult) return findResult;
     return null;
   }
-  getFolderById(folderId: string): Folder | null
-  {
+  getFolderById(folderId: string): Folder | null {
     const findResult = this.portfolioObject
-    .flatMap(c => c.folders)
-    .find(f => f.id === folderId);
+      .flatMap((c) => c.folders)
+      .find((f) => f.id === folderId);
     if (findResult) return findResult;
     return null;
   }
-  getProjectById(projectId: string): Project | null
-  {
-    const findResult = this.portfolioObject
-    .flatMap(c => c.folders)
-    .flatMap(f => f.projects)
-    .find(p => p.id === projectId) || this.portfolioObject
-    .flatMap(c => c.projects)
-    .find(p => p.id === projectId);
+  getProjectById(projectId: string): Project | null {
+    const findResult =
+      this.portfolioObject
+        .flatMap((c) => c.folders)
+        .flatMap((f) => f.projects)
+        .find((p) => p.id === projectId) ||
+      this.portfolioObject
+        .flatMap((c) => c.projects)
+        .find((p) => p.id === projectId);
     if (findResult) return findResult;
     return null;
   }
-  getCategoryInfoByName(categoryName: string): CategoryInfo | null
-  {
-    const findResult = this.portfolioObject.find(c => c.name === categoryName)
+  getCategoryInfoByName(categoryName: string): CategoryInfo | null {
+    const findResult = this.portfolioObject.find(
+      (c) => c.name === categoryName
+    );
     if (findResult) {
       return {
         id: findResult.id,
@@ -132,9 +133,13 @@ export class PortfolioService {
     }
     return null;
   }
-  getFolderInfoByName(categoryName: string, folderName: string): FolderInfo | null
-  {
-    const findResult = this.getCategoryFolders(categoryName).find(f => f.name === folderName);
+  getFolderInfoByName(
+    categoryName: string,
+    folderName: string
+  ): FolderInfo | null {
+    const findResult = this.getCategoryFolders(categoryName).find(
+      (f) => f.name === folderName
+    );
     if (findResult) {
       return {
         id: findResult.id,
@@ -146,15 +151,19 @@ export class PortfolioService {
     }
     return null;
   }
-  getProjectInfoByName(categoryName: string, projectName: string, folderName: string | null = null): ProjectInfo | null
-  {
-    const findResult = (folderName) ? this.portfolioObject
-    .find(c => c.name === categoryName)?.folders
-    .find(f => f.name === folderName)?.projects
-    .find(p => p.name === projectName)
-    : this.portfolioObject
-    .find(c => c.name === categoryName)?.projects
-    .find(p => p.name === projectName);
+  getProjectInfoByName(
+    categoryName: string,
+    projectName: string,
+    folderName: string | null = null
+  ): ProjectInfo | null {
+    const findResult = folderName
+      ? this.portfolioObject
+          .find((c) => c.name === categoryName)
+          ?.folders.find((f) => f.name === folderName)
+          ?.projects.find((p) => p.name === projectName)
+      : this.portfolioObject
+          .find((c) => c.name === categoryName)
+          ?.projects.find((p) => p.name === projectName);
     if (findResult) {
       return {
         id: findResult.id,
@@ -163,19 +172,20 @@ export class PortfolioService {
         title: findResult.title,
         objectType: PortfolioObjectType.ProjectInfo,
         thumbnail: findResult.thumbnail,
-        description: findResult.description
+        description: findResult.description,
       };
     }
     return null;
   }
-  getProjectsFolderByName(categoryName: string, folderName: string): ProjectInfo[] | null
-  {
+  getProjectsFolderByName(
+    categoryName: string,
+    folderName: string
+  ): ProjectInfo[] | null {
     let projects: ProjectInfo[] = [];
     const findResult = this.portfolioObject
-    .find(c => c.name === categoryName)?.folders
-    .find(f => f.name === folderName)?.projects;
-    for (const project of findResult? findResult : [])
-    {
+      .find((c) => c.name === categoryName)
+      ?.folders.find((f) => f.name === folderName)?.projects;
+    for (const project of findResult ? findResult : []) {
       projects.push({
         id: project.id,
         name: project.name,
@@ -183,45 +193,44 @@ export class PortfolioService {
         icon: project.icon,
         objectType: PortfolioObjectType.ProjectInfo,
         thumbnail: project.thumbnail,
-        description: project.description
-      })
+        description: project.description,
+      });
     }
-    return projects ? projects : null
+    return projects ? projects : null;
   }
-  getRandomProjects(count: number): Project[]
-  {
-    const mustContain: Project[] = [project_password_wallet];
-    let projects     : Project[] = [];
-    let randProjects : Project[] = [...mustContain];
-    let randIndices  : number [] = [];
-    this.portfolioObject.flatMap(c => c.folders).map(f => projects.push(...f.projects));
-    this.portfolioObject.flatMap(c => c.projects).map(p => projects.push(p));
-    let pending = (count <= projects.length) ? count : projects.length;
-    while ((pending - mustContain.length) > 0)
-    {
+  getRandomProjects(count: number, exclude: Project[]): Project[] {
+    let projects: Project[] = [];
+    let randProjects: Project[] = [];
+    let randIndices: number[] = [];
+    this.portfolioObject
+      .flatMap((c) => c.folders)
+      .map((f) => projects.push(...f.projects));
+    this.portfolioObject
+      .flatMap((c) => c.projects)
+      .map((p) => projects.push(p));
+    let pending = count <= projects.length ? count : projects.length;
+    while (pending > 0) {
       const randIdx: number = Math.floor(Math.random() * projects.length);
-      if (!randIndices.includes(randIdx))
-      {
+      if (
+        !randIndices.includes(randIdx) &&
+        !exclude.includes(projects[randIdx])
+      ) {
         randIndices.push(randIdx);
         --pending;
       }
     }
-    randIndices.map(i => randProjects.push(projects.filter(p => !mustContain.includes(p))[i]));
+    randIndices.map((i) => randProjects.push(projects[i]));
     return randProjects;
   }
-  getProjectPath(projectId: string): string[] | null
-  {
-    for (const category of this.portfolioObject)
-    {
-      for (const project of category.projects)
-      {
+  getProjectPath(projectId: string): string[] | null {
+    for (const category of this.portfolioObject) {
+      for (const project of category.projects) {
         if (project.id === projectId) return [category.name, project.name];
       }
-      for (const folder of category.folders)
-      {
-        for (const project of folder.projects)
-        {
-          if (project.id === projectId) return [category.name, folder.name, project.name];
+      for (const folder of category.folders) {
+        for (const project of folder.projects) {
+          if (project.id === projectId)
+            return [category.name, folder.name, project.name];
         }
       }
     }
